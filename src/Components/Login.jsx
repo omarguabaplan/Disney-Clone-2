21640623f4bec189e6c7ffce63c3ddb5971e667c
omarguabaplan <84946006+omarguabaplan@users.noreply.github.com>
2022-03-07 16:31:16 -0400

    Create Login.jsx

---
+++ src/Components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+
+export default function Login () {
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
